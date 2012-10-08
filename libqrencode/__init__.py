6fbb0d1371ca9dc1266cac25f4682fd636be431d
Matthew Baker <mat@mat-Lemur-Ultra.(none)>
2012-10-07 21:32:52 -0600

    moved project onto github.

---
+++ libqrencode/__init__.py
@@ -0,0 +1,20 @@
+"""This file is part of libqrencode python ctypes bindings.
+
+Copyright (C) 2012 Matthew Baker <mu.beta.06@gmail.com>
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the LGNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the LGNU Lesser General Public License
+along with this software.  If not, see <http://www.gnu.org/licenses/>."""
+
+author = 'Matthew Baker'
+version = '1.1'
+
